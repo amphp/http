de382166683da9ffed7cdae3266c4b1bae1f96ac
Niklas Keller <me@kelunik.com>
2018-01-20 11:16:00 +0100

    Implement header formatting

--- src/Parser/Rfc7230.php
+++ src/Parser/Rfc7230.php
@@ -9,6 +9,8 @@ namespace Amp\Http\Parser;
  */
 final class Rfc7230 {
     // We make use of possessive modifiers, which gives a slight performance boost
+    const HEADER_NAME_REGEX = "(^([^()<>@,;:\\\"/[\]?={}\x01-\x20\x7F]++)$)";
+    const HEADER_VALUE_REGEX = "(^[ \t]*+((?:[ \t]*+[\x21-\x7E\x80-\xFF]++)*+)[ \t]*+$)";
     const HEADER_REGEX = "(^([^()<>@,;:\\\"/[\]?={}\x01-\x20\x7F]++):[ \t]*+((?:[ \t]*+[\x21-\x7E\x80-\xFF]++)*+)[ \t]*+\r\n)m";
     const HEADER_FOLD_REGEX = "(\r\n[ \t]++)";
 
@@ -31,7 +33,7 @@ final class Rfc7230 {
         $count = \preg_match_all(self::HEADER_REGEX, $rawHeaders, $matches, \PREG_SET_ORDER);
 
         // If these aren't the same, then one line didn't match and there's an invalid header.
-        if ($count !== \substr_count($rawHeaders, "\r\n")) {
+        if ($count !== \substr_count($rawHeaders, "\n")) {
             // Folding is deprecated, see https://tools.ietf.org/html/rfc7230#section-3.2.4
             if (\preg_match(self::HEADER_FOLD_REGEX, $rawHeaders)) {
                 throw new ParseException("Invalid Header Syntax: Obsolete Line Folding");
@@ -50,5 +52,40 @@ final class Rfc7230 {
         }
 
         return $headers;
+    }
+
+    /**
+     * Format headers in to their on-the-wire format.
+     *
+     * **Important**: Always validate headers syntactically before calling this method. We have assertions in place, but
+     * these might be disabled on production and are no protection against response splitting when turned off. Headers
+     * should usually be verified when set on a response object, thus there's no reason to validate them again here.
+     *
+     * @param array $headers Headers in a format as returned by {@see parseHeaders()}.
+     *
+     * @return string Formatted headers.
+     */
+    public static function formatHeaders(array $headers): string {
+        $buffer = "";
+
+        foreach ($headers as $name => $values) {
+            // PHP casts integer-like keys to integers
+            $name =  (string) $name;
+
+            // Ignore any HTTP/2 pseudo headers
+            if ($name[0] === ":") {
+                continue;
+            }
+
+            \assert(\preg_match(self::HEADER_NAME_REGEX, $name), "Invalid header name");
+            \assert(\is_array($values));
+
+            foreach ($values as $value) {
+                \assert(\preg_match(self::HEADER_VALUE_REGEX, $value), "Invalid header value");
+                $buffer .= "{$name}: {$value}\r\n";
+            }
+        }
+
+        return $buffer;
     }
 }
--- test/Parser/Rfc7230Test.php
+++ test/Parser/Rfc7230Test.php
@@ -6,24 +6,29 @@ use PHPUnit\Framework\TestCase;
 
 class Rfc7230Test extends TestCase {
     /** @dataProvider provideValidHeaders */
-    public function testValidHeaders(string $rawHeaders, array $expectedResult) {
+    public function testValidHeaderParsing(string $rawHeaders, array $expectedResult) {
         $result = Rfc7230::parseHeaders($rawHeaders);
+        $this->assertSame($result, $expectedResult);
+    }
+
+    /** @dataProvider provideValidHeaders */
+    public function testValidHeaderFormatting(string $rawHeaders /* ignored for this case */, array $expectedResult) {
+        $result = Rfc7230::parseHeaders(Rfc7230::formatHeaders($expectedResult));
         $this->assertSame($result, $expectedResult);
     }
 
     public function provideValidHeaders() {
         return [
             ["x:y\r\n", ["x" => ["y"]]],
-            ["x:z\n", ["x" => ["z"]]],
             ["server:\tamphp.org\r\n", ["server" => ["amphp.org"]]],
             ["server: \tamphp.org  \t \r\n", ["server" => ["amphp.org"]]],
-            ["server: \tamphp.org  \t \r\nServer: amphp.org\n", ["server" => ["amphp.org", "amphp.org"]]],
+            ["server: \tamphp.org  \t \r\nServer: amphp.org\r\n", ["server" => ["amphp.org", "amphp.org"]]],
             ["ser124ver:\tamphp.org\r\n", ["ser124ver" => ["amphp.org"]]],
         ];
     }
 
     /** @dataProvider provideInvalidHeaders */
-    public function testInvalidHeaders(string $rawHeaders) {
+    public function testInvalidHeaderParsing(string $rawHeaders) {
         $this->expectException(ParseException::class);
         Rfc7230::parseHeaders($rawHeaders);
     }
@@ -31,7 +36,10 @@ class Rfc7230Test extends TestCase {
     public function provideInvalidHeaders() {
         return [
             [" x:y\r\n"],
+            ["x :z\r\n"],
+            [" x:z\r\n"],
             ["x :z\n"],
+            ["x:z\n"],
             ["ser ver:\tamphp.org\r\n"],
             ["server:\tamphp.org\r\n fold\r\n"],
         ];
