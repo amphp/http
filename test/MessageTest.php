c1317efc6b88df3c18580167b75a8a138134f3e3
Aaron Piotrowski <aaron@trowski.com>
2019-02-10 00:38:09 -0600

    Update Message test stub

--- test/MessageTest.php
+++ test/MessageTest.php
@@ -11,6 +11,26 @@ class TestMessage extends Message
     {
         $this->setHeaders($headers);
     }
+
+    public function setHeaders(array $headers)
+    {
+        parent::setHeaders($headers);
+    }
+
+    public function setHeader(string $name, $value)
+    {
+        parent::setHeader($name, $value);
+    }
+
+    public function addHeader(string $name, $value)
+    {
+        parent::addHeader($name, $value);
+    }
+
+    public function removeHeader(string $name)
+    {
+        parent::removeHeader($name);
+    }
 }
 
 class MessageTest extends TestCase
