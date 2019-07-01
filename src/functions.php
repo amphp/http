3109d41488e2724b92bde01c4333b0919b852f05
Niklas Keller <me@kelunik.com>
2019-07-01 20:28:11 +0200

    Rename and return raw pairs

--- src/functions.php
+++ src/functions.php
@@ -10,7 +10,7 @@ namespace Amp\Http;
  *
  * @see https://tools.ietf.org/html/rfc7230#section-3.2.6
  */
-function parseTokenListHeader(Message $message, string $headerName)
+function parseFieldValueComponents(Message $message, string $headerName)
 {
     $header = \implode(', ', $message->getHeaderArray($headerName));
 
@@ -26,15 +26,15 @@ function parseTokenListHeader(Message $message, string $headerName)
     foreach ($matches as $match) {
         $totalMatchedLength += \strlen($match[0]);
 
-        // case-insensitive, see https://tools.ietf.org/html/rfc7234.html#section-5.2
-        $key = \strtolower(\trim($match[1]));
+        $key = \trim($match[1]);
         $value = ($match[2] ?? '') . \trim($match[3] ?? '');
 
         if (($match[2] ?? '') !== '') {
+            // decode escaped characters
             $value = \preg_replace('(\\\\(.))', '\1', $value);
         }
 
-        $pairs[$key] = $value;
+        $pairs[] = [$key, $value];
     }
 
     if ($totalMatchedLength !== \strlen($header)) {
