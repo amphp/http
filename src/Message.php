6fce74a8aca9d25a80226a85f554f520a645fe71
Aaron Piotrowski <aaron@trowski.com>
2023-02-15 14:32:54 -0600

    Remove Status and Message aliases

--- src/Message.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Amp\Http;
-
-/**
- * Define a class alias for {@see HttpMessage} for easier upgrading.
- */
-\class_alias(HttpMessage::class, Message::class);
--- src/Status.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Amp\Http;
-
-/**
- * Define a class alias for {@see HttpStatus} for easier upgrading.
- */
-\class_alias(HttpStatus::class, Status::class);
