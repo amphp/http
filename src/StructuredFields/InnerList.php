efb153325dcb08538404c80bc3cf138099cae3a2
Aaron Piotrowski <aaron@trowski.com>
2024-01-14 11:12:12 -0600

    Constrain allowed types to InnerList

--- src/StructuredFields/InnerList.php
+++ src/StructuredFields/InnerList.php
@@ -3,13 +3,14 @@
 namespace Amp\Http\StructuredFields;
 
 /**
+ * @psalm-import-type Rfc8941SingleItem from Rfc8941
  * @psalm-import-type Rfc8941Parameters from Rfc8941
  * @extends Item<list<Item<scalar>>>
  */
 final class InnerList extends Item
 {
     /**
-     * @psalm-param list<Item<scalar>> $item
+     * @psalm-param list<Rfc8941SingleItem> $item
      * @psalm-param Rfc8941Parameters $parameters
      */
     public function __construct(array $item, array $parameters)
